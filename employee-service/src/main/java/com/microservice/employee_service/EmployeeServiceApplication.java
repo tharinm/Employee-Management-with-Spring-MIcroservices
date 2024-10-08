056fc85856ca53b9b57a0d3e04ba7085cc04b66f
tharinm <tharindumadhushan864@gmail.com>
2024-10-09 00:34:44 +0530

    discovery client added

--- employee-service/src/main/java/com/microservice/employee_service/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/microservice/employee_service/EmployeeServiceApplication.java
@@ -2,6 +2,7 @@ package com.microservice.employee_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +10,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
