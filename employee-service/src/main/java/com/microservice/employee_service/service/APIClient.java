c5c29af8affe8e73cbc239f01a2bcfa8a0329c93
tharinm <tharindumadhushan864@gmail.com>
2024-10-10 22:11:30 +0530

    api-gate-way-setup

--- employee-service/src/main/java/com/microservice/employee_service/service/APIClient.java
+++ employee-service/src/main/java/com/microservice/employee_service/service/APIClient.java
@@ -6,7 +6,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(url="http://localhost:8080" ,value="DEPARTMENT-SERVICE")
+//@FeignClient(url="http://localhost:8080" ,value="DEPARTMENT-SERVICE")
+@FeignClient(name="department-service")
 public interface APIClient {
 
 
