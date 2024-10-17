e21b04f40d5472cda95adb07545a66023ea16992
tharinm <tharindumadhushan864@gmail.com>
2024-10-17 13:36:37 +0530

    note that fro microserevice need to add config client not sonfig server

--- department-service/src/main/java/com/microservice/department_service/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/microservice/department_service/DepartmentServiceApplication.java
@@ -1,11 +1,14 @@
 package com.microservice.department_service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
 @SpringBootApplication
 @EnableDiscoveryClient
+
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
